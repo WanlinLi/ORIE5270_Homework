88d45202b604a48add377f7011d63052efb245de
WanlinLi <wl596@cornell.edu>
2018-08-30 19:56:34 -0400

    update

--- Homework4/Multiplication/multiply_folder/multiplication.py
+++ Homework4/Multiplication/multiply_folder/multiplication.py
@@ -2,36 +2,27 @@ from pyspark import SparkContext
 
 class multiplication(object):
     def add_index_matrix(self, l):
-        for j in range(len(l)):
-            yield (l[j], j)
+        for j in range(len(l)): yield (l[j], j)
 
     def add_index_vector(self, l):
-        for j in range(len(l)):
-            yield (j, l[j])
+        for j in range(len(l)): yield (j, l[j])
 
     def spark_multiplication(self, matrix_file, vector_file):
         """
-
         :param matrix_file: the txt file which contains the matrix, the first element of each row is the index
         :param vector_file: the txt file which contains the vector
         :return: a rdd object which contains the multiplication result (a vector)
         """
-        sc = SparkContext('local[2]', 'pyspark matrix mult')
+        sc = SparkContext('local[2]', 'hw4 mult mat')
         A = sc.textFile(matrix_file).map(eval)
-        A = A.map(lambda l: (l[0], list(l[1:-1])))
+        A = A.map(lambda l: (l[0], list(l[1:])))
         A = A.flatMapValues(self.add_index_matrix)
         A = A.map(lambda l: (l[1][1], (l[0], l[1][0])))
-
         a = sc.textFile(vector_file).map(eval)
         a = a.flatMap(self.add_index_vector)
         r = A.join(a)
         r = r.map(lambda l: (l[0], (l[1][0][0], l[1][1] * l[1][0][1])))
         r = r.map(lambda l: l[1])
-        r = r.reduceByKey(lambda n1, n2: n1 + n2).map(lambda l: l[1])
-
-        return r
-
-if __name__ == "__main__":
-    M = multiplication()
-    x = M.spark_multiplication('../test_folder/A1.txt','../test_folder/B1.txt')
-    print(x.collect())
+        r = r.reduceByKey(lambda n1, n2: n1 + n2)
+        r = r.map(lambda l: l[1])
+        return r, sc
--- Homework4/Multiplication/test_folder/test_multi.py
+++ Homework4/Multiplication/test_folder/test_multi.py
@@ -1,16 +1,21 @@
 import unittest
 from multiply_folder.multiplication import multiplication
 
+
 class testmultiple(unittest.TestCase):
     def test1(self):
         M = multiplication()
-        self.multi1 = M.spark_multiplication('./A1.txt', './B1.txt')
-	self.multi1 = self.multi1.collect()
+        self.multi1, sc = M.spark_multiplication('./test_folder/A1.txt', './test_folder/B1.txt')
+        self.multi1 = self.multi1.collect()
+        sc.stop()
         self.answer1 = [68, 167]
         assert self.multi1 == self.answer1
 
     def test2(self):
         M = multiplication()
-        self.multi2 = M.spark_multiplication('A2.txt', 'B2.txt').collect()
+        self.multi2, sc = M.spark_multiplication('./test_folder/A2.txt', './test_folder/B2.txt')
+        self.multi2 = self.multi2.collect()
+        sc.stop()
         self.answer2 = [3535, 193, 536, -3782]
         assert self.multi2 == self.answer2
+
